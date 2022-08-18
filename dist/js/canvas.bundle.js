1a06ba2d0a973f9ca6cf630840ac02fd5bd386c6
Gloria Kitchens <gmkforever1@gmail.com>
2022-08-17 21:45:23 -0700

    Collision Detection Part 2

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -97,12 +97,6 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
 
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
@@ -112,7 +106,7 @@ var mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 };
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']; // Event Listeners
+var colors = ['#2185C5', '#7ECEFD', '#bdb29f', '#FF7F66']; // Event Listeners
 
 addEventListener('mousemove', function (event) {
   mouse.x = event.clientX;
@@ -122,44 +116,136 @@ addEventListener('resize', function () {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
   init();
-}); // Objects
+}); // Utils
 
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
+function rotate(velocity, angle) {
+  var rotatedVelocities = {
+    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+  };
+  return rotatedVelocities;
+}
 
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+function resolveCollision(particle, otherParticle) {
+  var xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+  var yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+  var xDist = otherParticle.x - particle.x;
+  var yDist = otherParticle.y - particle.y;
+
+  if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+    var angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+    var m1 = particle.mass;
+    var m2 = otherParticle.mass;
+    var u1 = rotate(particle.velocity, angle);
+    var u2 = rotate(otherParticle.velocity, angle);
+    var v1 = {
+      x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2),
+      y: u1.y
+    };
+    var v2 = {
+      x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2),
+      y: u2.y
+    };
+    var vFinal1 = rotate(v1, -angle);
+    var vFinal2 = rotate(v2, -angle);
+    particle.velocity.x = vFinal1.x;
+    particle.velocity.y = vFinal1.y;
+    otherParticle.velocity.x = vFinal2.x;
+    otherParticle.velocity.y = vFinal2.y;
   }
+} // Objects
 
-  _createClass(Object, [{
-    key: "draw",
-    value: function draw() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-      c.closePath();
+
+function Particle(x, y, radius, color) {
+  var _this = this;
+
+  this.x = x;
+  this.y = y;
+  this.velocity = {
+    x: (Math.random() - 0.5) * 2,
+    y: (Math.random() - 0.5) * 2
+  };
+  this.radius = radius;
+  this.color = color;
+  this.mass = 1;
+  this.opacity = 0;
+
+  this.draw = function () {
+    c.beginPath();
+    c.arc(_this.x, _this.y, _this.radius, 0, Math.PI * 2, false);
+    c.save();
+    c.globalAlpha = _this.opacity;
+    c.fillStyle = _this.color;
+    c.fill();
+    c.restore();
+    c.strokeStyle = _this.color;
+    c.stroke();
+    c.closePath();
+  };
+
+  this.update = function (particles) {
+    _this.draw(); // interaction of a particle with other particles
+
+
+    for (var i = 0; i < particles.length; i++) {
+      if (_this === particles[i]) continue;
+
+      if (Object(_utils__WEBPACK_IMPORTED_MODULE_0__["distance"])(_this.x, _this.y, particles[i].x, particles[i].y) - _this.radius * 2 < 0) {
+        resolveCollision(_this, particles[i]);
+      }
+    } // actions of single particle
+
+
+    if (_this.x + _this.radius >= innerWidth || _this.x - _this.radius <= 0) {
+      _this.velocity.x = -_this.velocity.x;
     }
-  }, {
-    key: "update",
-    value: function update() {
-      this.draw();
+
+    if (_this.y + _this.radius >= innerHeight || _this.y - _this.radius <= 0) {
+      _this.velocity.y = -_this.velocity.y;
     }
-  }]);
 
-  return Object;
-}(); // Implementation
+    if (Object(_utils__WEBPACK_IMPORTED_MODULE_0__["distance"])(mouse.x, mouse.y, _this.x, _this.y) < 50) {
+      _this.opacity += 0.05;
+
+      if (_this.opacity > 0.5) {
+        _this.opacity = 0.5;
+      }
+    } else {
+      _this.opacity -= 0.05;
+
+      if (_this.opacity < 0) {
+        _this.opacity = 0;
+      }
+    }
+
+    _this.x += _this.velocity.x;
+    _this.y += _this.velocity.y;
+  };
+} // Implementation
 
 
-var objects;
+var particles;
 
 function init() {
-  objects = [];
+  particles = [];
 
-  for (var i = 0; i < 400; i++) {// objects.push()
+  for (var i = 0; i < 400; i++) {
+    var radius = 10;
+    var x = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(radius, canvas.width - radius);
+    var y = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(radius, canvas.height - radius);
+    var color = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomColor"])(colors);
+
+    if (i !== 0) {
+      for (var j = 0; j < particles.length; j++) {
+        if (Object(_utils__WEBPACK_IMPORTED_MODULE_0__["distance"])(x, y, particles[j].x, particles[j].y) - radius * 2 < 0) {
+          x = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(radius, canvas.width - radius);
+          y = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(radius, canvas.height - radius);
+          j = -1;
+        }
+      }
+    }
+
+    particles.push(new Particle(x, y, radius, color));
   }
 } // Animation Loop
 
@@ -167,9 +253,9 @@ function init() {
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  particles.forEach(function (particle) {
+    particle.update(particles);
+  });
 }
 
 init();
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,4 +1,4 @@
-import utils from './utils'
+import utils, { distance, randomColor, randomIntFromRange } from './utils'
 
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
@@ -11,7 +11,7 @@ const mouse = {
   y: innerHeight / 2
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+const colors = ['#2185C5', '#7ECEFD', '#bdb29f', '#FF7F66']
 
 // Event Listeners
 addEventListener('mousemove', (event) => {
@@ -26,35 +26,134 @@ addEventListener('resize', () => {
   init()
 })
 
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
+// Utils
+function rotate(velocity, angle) {
+  const rotatedVelocities = {
+    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+  };
 
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
+  return rotatedVelocities;
+}
 
-  update() {
-    this.draw()
+function resolveCollision(particle, otherParticle) {
+  const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+  const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+
+  const xDist = otherParticle.x - particle.x;
+  const yDist = otherParticle.y - particle.y;
+
+  if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+
+    const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+    const m1 = particle.mass;
+    const m2 = otherParticle.mass;
+
+    const u1 = rotate(particle.velocity, angle);
+    const u2 = rotate(otherParticle.velocity, angle);
+
+    const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x *2 * m2 / (m1 + m2), y: u1.y };
+    const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+
+    const vFinal1 = rotate(v1, -angle);
+    const vFinal2 = rotate(v2, -angle);
+
+    particle.velocity.x = vFinal1.x;
+    particle.velocity.y = vFinal1.y;
+
+    otherParticle.velocity.x = vFinal2.x;
+    otherParticle.velocity.y = vFinal2.y;
   }
 }
 
+// Objects
+function Particle(x, y, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.velocity = {
+    x: (Math.random() - 0.5) * 2,
+    y: (Math.random() - 0.5) * 2
+  }
+  this.radius = radius;
+  this.color = color;
+  this.mass = 1;
+  this.opacity = 0;
+
+  this.draw = () => {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.save();
+    c.globalAlpha = this.opacity;
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+    c.strokeStyle = this.color
+    c.stroke()
+    c.closePath()
+  };
+
+  this.update = particles => {
+    this.draw();
+
+    // interaction of a particle with other particles
+    for (let i = 0; i < particles.length; i++) {
+      if (this === particles[i]) continue;
+      if (distance (this.x, this.y, particles[i].x, particles[i].y) - this.radius * 2 < 0) {
+        resolveCollision(this, particles[i]);
+      }
+    }
+
+    // actions of single particle
+    if (this.x + this.radius >= innerWidth || this.x - this.radius <= 0) {
+      this.velocity.x = -this.velocity.x;
+    }
+
+    if (this.y + this.radius >= innerHeight || this.y - this.radius <= 0) {
+      this.velocity.y = -this.velocity.y;
+    }
+
+    if(distance(mouse.x, mouse.y, this.x, this.y) < 50) {
+      this.opacity += 0.05;
+      if (this.opacity > 0.5) {
+        this.opacity = 0.5;
+      }
+
+    } else {
+      this.opacity -= 0.05;
+      if (this.opacity < 0) {
+        this.opacity = 0;
+      }
+
+    }
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  };
+}
+
 // Implementation
-let objects
+let particles;
 function init() {
-  objects = []
+  particles = [];
 
   for (let i = 0; i < 400; i++) {
-    // objects.push()
+    const radius = 10;
+    let x = randomIntFromRange(radius, canvas.width - radius);
+    let y = randomIntFromRange(radius, canvas.height - radius);
+    const color = randomColor(colors);
+
+    if (i !== 0) {
+      for (let j = 0; j < particles.length; j++) {
+        if (distance (x, y, particles[j].x, particles[j].y) - radius * 2 < 0) {
+          x = randomIntFromRange(radius, canvas.width - radius);
+          y = randomIntFromRange(radius, canvas.height - radius);
+          j = -1;
+        }
+      }
+    }
+
+    particles.push(new Particle(x, y, radius, color));
   }
 }
 
@@ -63,10 +162,9 @@ function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  particles.forEach(particle => {
+    particle.update(particles);
+  });
 }
 
 init()
